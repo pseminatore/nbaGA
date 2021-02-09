cd984c92c620e1012f79823fc6c5337ac4b2ec08
Patrick <pseminatore@gmail.com>
2021-02-09 14:54:03 -0800

    Implemented base mating.  Base functionality done

--- nba_genetic_algorithm.py
+++ nba_genetic_algorithm.py
@@ -5,21 +5,73 @@ import random
 from tqdm import tqdm
 from itertools import zip_longest
 
-def main(df, population, max_generations, mutations_per_generation, max_salary, fitness_strategy, mating_strategy):
+def main(df, population, max_generations, mutations_per_generation, max_salary, fitness_strategy, pairing_strategy, mating_strategy):
     
     print("Darwinning...")
     curr_population = population
+    ### TODO -- Include some sort of cutoff when result is good enough
     for i in tqdm(range(max_generations)):
         ## Calculate fitness
         population_fitness_tuple = calculate_fitness(curr_population, df, max_salary, fitness_strategy)
         ## Determine mating pool
         sorted_population_tuple = sorted(population_fitness_tuple, key=lambda item: item[0], reverse=True)
         ## Pair up parents
-        paired_population_tuple = pair_parents(sorted_population_tuple, mating_strategy)
+        paired_population_tuple = pair_parents(sorted_population_tuple, pairing_strategy)
         ## Mating Crossover
         ## Mutation
+        curr_population = mate_parents(paired_population_tuple, mating_strategy, mutations_per_generation)
         ## new_generation = get offspring
+    population_fitness_tuple = calculate_fitness(curr_population, df, max_salary, fitness_strategy)
+    sorted_population_tuple = sorted(population_fitness_tuple, key=lambda item: item[0], reverse=True)
+    team = extract_team(sorted_population_tuple[0][1], df)
+    print("")
+    
+    ## Get best team (individual)
+    ## Build starting 5
+    ## Display Roster
 
+def mate_parents(paired_population_tuple, mating_strategy, mutations_per_generation):
+    ### TODO -- Add mutation
+    if mating_strategy == 0:
+        child_population = base_mating(paired_population_tuple)
+        
+    return child_population
+
+
+def base_mating(paired_population_tuple):
+    new_generation = []
+    for mating_pair in paired_population_tuple:
+        
+        ## Extract mother and father genomes
+        mother = mating_pair[0][1]
+        father = mating_pair[1][1]
+        
+        ## Crossover mating
+        crossover_idx = random.randint(0, 14)
+        
+        mother_gene_idcs = list(np.where(mother == 1)[0])
+        father_gene_idcs = list(np.where(father == 1)[0])
+        
+        child1_idcs = mother_gene_idcs[:crossover_idx]
+        child1_idcs += father_gene_idcs[crossover_idx:]
+        
+        child2_idcs = father_gene_idcs[:crossover_idx]
+        child2_idcs += mother_gene_idcs[crossover_idx:]
+        
+        ## Blank Child arrays
+        child1 = np.zeros(403)
+        child2 = np.zeros(403)
+        
+        ## Set the player indices to 1
+        np.put(child1, child1_idcs, 1)
+        np.put(child2, child2_idcs, 1)
+        
+        ## Add children to new generation
+        new_generation.append(child1)
+        new_generation.append(child2)
+        
+    return new_generation
+        
 def init_population(pop_size, df, max_salary):
     population = []
     
@@ -38,9 +90,9 @@ def init_population(pop_size, df, max_salary):
         
     return population
 
-def pair_parents(sorted_population_tuple, mating_strategy):
+def pair_parents(sorted_population_tuple, pairing_strategy):
     paired_tuple = []
-    if mating_strategy == 0:
+    if pairing_strategy == 0:
         parent_pairs = top_down_pairing(sorted_population_tuple)
     return parent_pairs
 
@@ -140,6 +192,20 @@ def extract_salaries(individual, df):
         salaries[player_row.Name] = int_salary # For debugging purposes -- and it's kinda fun to see what teams get built
         
     return total_salary
+
+def extract_team(individual, df):
+    players = []
+    player_indices = np.where(individual == 1)[0]
+    for player_index in player_indices:
+        player_row = df.iloc[player_index]
+        name = player_row.Name
+        position = player_row.Position
+        rating = player_row.Rating
+        salary = player_row.Salary
+        player = [name, rating, position, salary]
+        players.append(player)
+        
+    return players
     
 if __name__ == "__main__":
     df = create_dataframe.create()
@@ -148,5 +214,6 @@ if __name__ == "__main__":
     max_generations = 70
     mutations_per_generation = 1
     fitness_strategy = 0
+    pairing_strategy = 0
     mating_strategy = 0
-    main(df, population, max_generations, mutations_per_generation, max_salary, fitness_strategy, mating_strategy)
+    main(df, population, max_generations, mutations_per_generation, max_salary, fitness_strategy, pairing_strategy, mating_strategy)
