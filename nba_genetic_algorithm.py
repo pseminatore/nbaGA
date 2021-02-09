37b91d3c3ed72a5b76aa13b856315fae22c527b6
Patrick <pseminatore@gmail.com>
2021-02-08 19:51:21 -0800

    extracted total salary from individual

--- nba_genetic_algorithm.py
+++ nba_genetic_algorithm.py
@@ -10,6 +10,7 @@ def main(population, max_generations, mutations_per_generation, max_salary):
     curr_population = population
     for i in tqdm(range(max_generations)):
         ## Calculate fitness
+        calculate_fitness(curr_population, df, max_salary)
         ## Determine mating pool
         ## Pair up parents
         ## Mating Crossover
@@ -36,20 +37,48 @@ def get_random_indices(roster_size=15):
     
     players = []   
     for roster_spot in range(roster_size):
+        
         while True:
+            
             add_player_index = random.randint(0, 402)
+            
             if add_player_index not in players:
+                
                 players.append(add_player_index)
                 break
             
     return players
 
 
-def calculate_fitness(population, df):
+def calculate_fitness(population, df, max_salary):
     fitness_dict = {}
     for individual in population:
+        total_salary = extract_salaries(individual, df)
         
+        ### TODO -- Continue to calculate fitness
+        if total_salary > max_salary:
+            fitness = 0
+            
+        pass
         
+
+def extract_salaries(individual, df):
+    salaries = {}
+    total_salary = 0
+    player_indices = np.where(individual == 1)[0]
+    
+    for player_index in player_indices.tolist():
+        
+        player_row = df.iloc[player_index]
+        salary = player_row.Salary
+        
+        int_salary = int(salary.replace(',', ''))
+        total_salary += int_salary    
+        
+        salaries[player_row.Name] = int_salary # For debugging purposes -- and it's kinda fun to see what teams get built
+        
+    return total_salary
+    
 if __name__ == "__main__":
     population = init_population(100)
     max_generations = 70
